e465015191baba02e345781b2ca4f39001fa82f1
mrnnasir <raihan80@gmail.com>
2025-10-22 22:00:41 +0200

    Created, updated codes and comments

---
+++ js/app.js
@@ -0,0 +1,94 @@
+let fullMeal = [];
+let mealCategory = [];
+
+let catName = document.getElementById("catName");//Input element
+let output = document.getElementById("output1"); //Div-For list of Category
+let mealSec = document.getElementById("mealSec"); //Div-For Filtered Category
+
+//Button
+let mealCatList = document.getElementById("mealCatList"); //For List of Category
+let mealCat = document.getElementById("mealCat"); //For Filtered Category
+
+//Fetching data using API
+//async function allows us to use 'await' inside the main() until a Promise is resolved.
+async function main() {
+  const resp = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=s")
+  const data = await resp.json()
+  if (data.meals && data.meals.length > 0) {
+    fullMeal=data.meals;
+  }
+}
+main();
+
+//List of Category---
+function getUniCat() {
+  const uniqueCat = new Set(); //Array of each unique value (category) will be stored in uniqueCat variable
+
+  //By using forEach method, we are adding each unique value from 'fullMeal' that meets the condition to uniqueCat variable, which is a Set().
+  fullMeal.forEach(meal => {
+    if (meal.strCategory) {
+      uniqueCat.add(meal.strCategory);
+    }
+  });
+  return [...uniqueCat];//Since uniqueCat is a Set, we need to convert this to an Array by using Spread Operator
+}
+
+function catList(msg) {
+  let outputDiv = document.createElement("div");
+  outputDiv.classList.add("outputD");
+
+  let outputP = document.createElement("p");
+  outputP.classList.add("outputCatP");
+
+  outputP.textContent = msg;
+  outputDiv.appendChild(outputP);
+  output.append(outputDiv);
+}
+
+mealCatList.addEventListener("click", function (){
+  let removeElement = document.getElementsByClassName("outputD");
+  [...removeElement].forEach(el => el.remove())
+
+  let count = 1;
+  mealCategory = getUniCat();
+  mealCategory.sort();
+  mealCategory.forEach(meal => {catList(count++ + '. ' + meal);});
+})
+
+//Filtered Category---
+function printScreen(msg) {
+  let mealDiv = document.createElement('div');
+  mealDiv.classList.add('mealNaD');
+
+  let mealP = document.createElement('p');
+  mealP.classList.add('mealNaP');
+
+  mealP.innerHTML = msg
+
+  mealDiv.appendChild(mealP)
+  mealSec.appendChild(mealDiv)
+}
+
+mealCat.addEventListener("click", function() {
+  let removeElement = document.getElementsByClassName('mealNaD');
+  [...removeElement].forEach(el => el.remove());
+
+  let userCat = catName.value.trim().toLowerCase()
+
+  let filterCategory = fullMeal.filter(cat => cat.strCategory.toLowerCase() === userCat);
+
+  if(userCat === '') {
+    printScreen('Oops! You forgot to enter category name. Please try again!');
+  }
+  else if (filterCategory.length === 0) {
+    printScreen(`Oops! <u>${userCat}</u> is a wrong name. Please enter a correct category name!`)
+  }
+  else {
+    let count = 1;
+    filterCategory.forEach(cat => {
+      //printScreen(count++ + '. ' + cat.strMeal + ': ' + cat.strCategory) --> General code
+      //To highlight the category within .js, I used style within span-tag
+      printScreen(`${count++}. ${cat.strMeal}: <span style="color: red;">${cat.strCategory}</span>`)
+    })
+  }
+})

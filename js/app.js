d4638c260534b1d160c0b1e5a9a9502084154e43
mrnnasir <raihan80@gmail.com>
2025-10-22 22:37:10 +0200

    Updated comment

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ async function main() {
 main();
 
 //List of Category---
+//To store Unique category value to a variable---
 function getUniCat() {
   const uniqueCat = new Set(); //Array of each unique value (category) will be stored in uniqueCat variable
 
@@ -33,53 +34,69 @@ function getUniCat() {
   return [...uniqueCat];//Since uniqueCat is a Set, we need to convert this to an Array by using Spread Operator
 }
 
+//To add the content in HTML
 function catList(msg) {
+  //Div will be created with class
   let outputDiv = document.createElement("div");
   outputDiv.classList.add("outputD");
 
+  //P-tag will be created with class
   let outputP = document.createElement("p");
   outputP.classList.add("outputCatP");
 
+  //msg argument will be added to the body of html
   outputP.textContent = msg;
   outputDiv.appendChild(outputP);
   output.append(outputDiv);
 }
 
+//To listen button function about the request---
 mealCatList.addEventListener("click", function (){
+  //To remove the collection past elements from the class of a previously declared Div 'outputD'
   let removeElement = document.getElementsByClassName("outputD");
   [...removeElement].forEach(el => el.remove())
 
   let count = 1;
-  mealCategory = getUniCat();
-  mealCategory.sort();
-  mealCategory.forEach(meal => {catList(count++ + '. ' + meal);});
+  mealCategory = getUniCat(); //List of categories will be stored in mealCategory
+  mealCategory.sort(); //Items in mealCategory will be alphabetically sorted
+  mealCategory.forEach(meal => {catList(count++ + '. ' + meal);}); //It will call the catList function to execute the operation
 })
 
 //Filtered Category---
+//To add the content in HTML
 function printScreen(msg) {
+  //Div will be created with class
   let mealDiv = document.createElement('div');
   mealDiv.classList.add('mealNaD');
 
+  //P-tag will be created with class
   let mealP = document.createElement('p');
   mealP.classList.add('mealNaP');
 
+  //msg argument will be added to the body of html
   mealP.innerHTML = msg
-
   mealDiv.appendChild(mealP)
   mealSec.appendChild(mealDiv)
 }
 
+//To listen button function about the request---
 mealCat.addEventListener("click", function() {
+  //To remove the collection past elements from the class of a previously declared Div 'mealNaD'
   let removeElement = document.getElementsByClassName('mealNaD');
   [...removeElement].forEach(el => el.remove());
 
+  //Store user input in lower case
   let userCat = catName.value.trim().toLowerCase()
 
+  //Check inserted user category with listed category in fullMeal then store the matched items in filterCategory
   let filterCategory = fullMeal.filter(cat => cat.strCategory.toLowerCase() === userCat);
 
+  //Applied condition for user's convenience
   if(userCat === '') {
     printScreen('Oops! You forgot to enter category name. Please try again!');
   }
+  //filterCategory.length === 0 -> means the item is not in the list. Hence, 0.
+  //If inserted user category name is spelled wrong, it will not be in the list. So, length will render 0.
   else if (filterCategory.length === 0) {
     printScreen(`Oops! <u>${userCat}</u> is a wrong name. Please enter a correct category name!`)
   }
